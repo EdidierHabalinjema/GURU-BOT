700da66045e79fdf92468bd5a1f86969ccddac83
EdidierHabalinjema <84735241+EdidierHabalinjema@users.noreply.github.com>
2023-11-17 01:43:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['265887728557', 'More Less Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
